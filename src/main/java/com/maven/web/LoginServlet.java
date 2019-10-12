f6b8d3678df1658f29349dcf3350b75d2896cfd5
zulfi <zulkar.shaikh@gmail.com>
2019-10-12 23:38:33 +0100

    initial

---
+++ src/main/java/com/maven/web/LoginServlet.java
@@ -0,0 +1,17 @@
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.PrintWriter;
+
+public class LoginServlet extends HttpServlet { 
+  protected void doGet(HttpServletRequest request, 
+      HttpServletResponse response) throws ServletException, IOException 
+  {
+    // reading the user input
+    String color= request.getParameter("color");    
+    PrintWriter out = response.getWriter();
+    out.println ("<html><body><h1>Hello Zulfi</h1></body</html");  
+  }  
+}
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,12 @@
+<html>
+<body>
+<h2>Hello World!</h2>
+
+<form action="LoginServlet">			
+			 Please enter a color <br>
+			<input type="text" name="color"size="20px">
+			<input type="submit" value="submit">						
+</form>
+
+</body>
+</html>
